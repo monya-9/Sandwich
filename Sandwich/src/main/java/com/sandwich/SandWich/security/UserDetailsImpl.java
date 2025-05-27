9b04bbab8358e20224a678b646c2996e0d904396
ghdtldus <ghdtldus03a@gmail.com>
2025-05-28 02:11:52 +0900

    login API / JWT / SecurityConfig

---
+++ Sandwich/src/main/java/com/sandwich/SandWich/security/UserDetailsImpl.java
@@ -0,0 +1,57 @@
+package com.sandwich.SandWich.security;
+
+import com.sandwich.SandWich.domain.User;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+
+@Getter
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    // 권한 없음으로 단순 처리 (지금은 ROLE 처리 안 하니까 비워둠)
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ Sandwich/src/main/java/com/sandwich/SandWich/service/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.sandwich.SandWich.service;
+
+import com.sandwich.SandWich.domain.User;
+import com.sandwich.SandWich.repository.UserRepository;
+import com.sandwich.SandWich.security.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자 없음"));
+
+        return new UserDetailsImpl(user);
+    }
+}
