502fa4ad1bddd63ad02074f0bc86a1da5fd99025
ghdtldus <ghdtldus03a@gmail.com>
2025-06-28 13:25:50 +0900

    feat : 사용자 프로필 로직 통일 구현

--- Sandwich/src/main/java/com/sandwich/SandWich/user/controller/UserController.java
+++ Sandwich/src/main/java/com/sandwich/SandWich/user/controller/UserController.java
@@ -1,17 +1,19 @@
 package com.sandwich.SandWich.user.controller;
 
-import com.sandwich.SandWich.global.exception.exceptiontype.ProfileAlreadyExistsException;
+import com.sandwich.SandWich.auth.security.UserDetailsImpl;
 import com.sandwich.SandWich.global.exception.exceptiontype.UserNotFoundException;
 import com.sandwich.SandWich.user.domain.User;
-import com.sandwich.SandWich.user.dto.SocialProfileRequest;
+import com.sandwich.SandWich.user.dto.UserProfileRequest;
 import com.sandwich.SandWich.user.repository.UserRepository;
 import com.sandwich.SandWich.user.service.UserService;
 import com.sandwich.SandWich.auth.security.JwtUtil;
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -26,6 +28,7 @@ public class UserController {
 
     // 마이페이지 조회
     @GetMapping("/me")
+    @Transactional
     public ResponseEntity<?> getMyProfile(Authentication authentication) {
         System.out.println("getName() = " + authentication.getName());
         String email = authentication.getName();
@@ -39,7 +42,7 @@ public class UserController {
     @DeleteMapping("/me")
     public ResponseEntity<?> deleteMyAccount(Authentication authentication) {
         String email = authentication.getName();
-        User user = userRepository.findByEmail(email)
+        User user = userRepository.findByEmailAndIsDeletedFalse(email)
                 .orElseThrow(() -> new RuntimeException("유저를 찾을 수 없습니다."));
 
         user.setDeleted(true);
@@ -49,23 +52,16 @@ public class UserController {
         return ResponseEntity.ok("회원 탈퇴 완료");
     }
 
-    // 소셜 로그인 후 프로필 등록
-    @PostMapping("/profile")
+
+    @PutMapping("/profile")
     @Transactional
-    public ResponseEntity<?> saveSocialProfile(@RequestBody SocialProfileRequest req,
-                                               @RequestHeader("Authorization") String token,
-                                               @RequestParam("provider") String provider) {
-        String email = jwtUtil.validateToken(token.replace("Bearer ", ""));
+    public ResponseEntity<?> updateOrCreateProfile(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @RequestBody @Valid UserProfileRequest req
+    ) {
+        User user = userDetails.getUser();
 
-        User user = userRepository.findByEmailAndProvider(email, provider)
-                .orElseThrow(() -> new RuntimeException("유저를 찾을 수 없습니다."));
-
-        if (user.getUserPosition() != null || (user.getInterests() != null && !user.getInterests().isEmpty())) {
-            throw new ProfileAlreadyExistsException();
-        }
-
-        userService.saveProfile(user, req);
-
-        return ResponseEntity.ok("소셜 유저 프로필 저장 완료");
+        userService.upsertUserProfile(user, req);
+        return ResponseEntity.ok("프로필 설정 완료");
     }
 }
---
+++ Sandwich/src/main/java/com/sandwich/SandWich/user/dto/UserProfileResponse.java
@@ -0,0 +1,24 @@
+package com.sandwich.SandWich.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Data
+public class UserProfileResponse {
+    private String username;
+    private String email;
+    private String bio;
+    private String skills;
+    private String github;
+    private String linkedin;
+    private String profileImage;
+    private PositionDto position;
+    private List<InterestDto> interests;
+    private int followerCount;
+    private int followingCount;
+
+
+}
