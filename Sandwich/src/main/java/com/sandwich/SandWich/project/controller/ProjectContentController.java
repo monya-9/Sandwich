b53fb5c0673804e8807022fccb9eaa28ec47f045
ghdtldus <ghdtldus03a@gmail.com>
2025-07-01 21:20:21 +0900

    feat : 콘텐츠 수정 API

--- Sandwich/src/main/java/com/sandwich/SandWich/project/controller/ProjectContentController.java
+++ Sandwich/src/main/java/com/sandwich/SandWich/project/controller/ProjectContentController.java
@@ -1,6 +1,7 @@
 package com.sandwich.SandWich.project.controller;
 
 import com.sandwich.SandWich.project.dto.ProjectContentRequest;
+import com.sandwich.SandWich.project.dto.ProjectContentUpdateRequest;
 import com.sandwich.SandWich.project.service.ProjectContentService;
 import com.sandwich.SandWich.user.domain.User;
 import lombok.RequiredArgsConstructor;
@@ -39,5 +40,16 @@ public class ProjectContentController {
         User user = userDetails.getUser();
         contentService.deleteContent(projectId, contentId, user);
         return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{contentId}")
+    public ResponseEntity<Void> updateContent(
+            @PathVariable Long projectId,
+            @PathVariable Long contentId,
+            @RequestBody ProjectContentUpdateRequest request,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        contentService.updateContent(projectId, contentId, request.getData(), userDetails.getUser());
+        return ResponseEntity.ok().build();
     }
 }
---
+++ Sandwich/src/main/java/com/sandwich/SandWich/project/dto/ProjectContentUpdateRequest.java
@@ -0,0 +1,8 @@
+package com.sandwich.SandWich.project.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ProjectContentUpdateRequest {
+    private String data;
+}
