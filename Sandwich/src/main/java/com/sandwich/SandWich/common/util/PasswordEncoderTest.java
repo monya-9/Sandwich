2cd451f0d5a0f4504e224977ae7f0c6d31008f69
ghdtldus <ghdtldus03a@gmail.com>
2025-08-01 09:46:03 +0900

    ãSecurityConfig 수정

--- Sandwich/src/main/java/com/sandwich/SandWich/common/util/PasswordEncoderTest.java
+++ Sandwich/src/main/java/com/sandwich/SandWich/common/util/PasswordEncoderTest.java
@@ -6,7 +6,7 @@ public class PasswordEncoderTest {
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-        String rawPassword = "ai1234"; // 여기 원하는 비번 입력
+        String rawPassword = "admin123!"; // 여기 원하는 비번 입력
         String encoded = encoder.encode(rawPassword);
 
         System.out.println("BCrypt 암호화 결과:");
