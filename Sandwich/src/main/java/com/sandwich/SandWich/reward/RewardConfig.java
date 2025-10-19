a2e886dffd21a3eddcc6a18c9215fed57f9393e6
ghdtldus <ghdtldus03a@gmail.com>
2025-10-19 23:07:48 +0900

    자동 발표 트리거 운영 설정

--- Sandwich/src/main/java/com/sandwich/SandWich/reward/RewardConfig.java
+++ Sandwich/src/main/java/com/sandwich/SandWich/reward/RewardConfig.java
@@ -4,5 +4,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties(RewardProperties.class)
+@EnableConfigurationProperties({
+        RewardProperties.class,
+        RewardAutoPublishProperties.class
+})
 public class RewardConfig {}
