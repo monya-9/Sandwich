335921fc63ad42932e60f1539a2f21cb12c64b1c
ghdtldus <ghdtldus03a@gmail.com>
2025-11-16 18:40:28 +0900

    없는 기능인 게시판(post)관련 모두 삭제

--- Sandwich/src/main/java/com/sandwich/SandWich/notification/events/CommentCreatedEvent.java
+++ Sandwich/src/main/java/com/sandwich/SandWich/notification/events/CommentCreatedEvent.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter @AllArgsConstructor
 public class CommentCreatedEvent {
     private final Long actorId;        // 댓글 단 사람
-    private final String resourceType; // "POST" | "PROJECT" | "CHALLENGE" (대문자 권장)
+    private final String resourceType; // "PROJECT" | "CHALLENGE" (대문자 권장)
     private final Long resourceId;     // 글/프로젝트/챌린지 ID
     private final Long targetUserId;   // 알림 받을 사용자
     private final String snippet;      // 내용 요약
